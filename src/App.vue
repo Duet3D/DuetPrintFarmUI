9e8740fdd4e268ce079e254eaaefd29c352cbe20
Christian Hammacher <bmasterc@gmail.com>
2021-11-16 16:49:40 +0100

    Rearranged jobs and printers

--- src/App.vue
+++ src/App.vue
@@ -43,113 +43,119 @@
 
 <template>
 	<div id="app">
-		<b-container class="mt-3">
-			<h1 class="mb-4 text-center">
-				Duet3D Print Farm Overview
-			</h1>
+        <b-container fluid class="my-3">
+            <h1 class="mb-4 text-center">
+                Duet3D Print Farm Overview
+            </h1>
 
-			<b-alert :show="!!errorMessage" variant="danger">
-				<b-icon icon="exclamation-triangle" class="mr-1"></b-icon> {{ errorMessage }}
-			</b-alert>
+            <b-alert :show="!!errorMessage" variant="danger">
+                <b-icon icon="exclamation-triangle" class="mr-1"></b-icon> {{ errorMessage }}
+            </b-alert>
 
-			<b-card no-body>
-				<template #header>
-					<span>
-						<b-icon icon="card-list"></b-icon> Job Queue
-					</span>
+            <b-row>
+                <b-col cols="8">
+                    <b-card no-body>
+                        <template #header>
+                            <span>
+                                <b-icon icon="card-list"></b-icon> Job Queue
+                            </span>
 
-					<div>
-						<upload-button :disabled="!!errorMessage"></upload-button>
+                            <div>
+                                <upload-button :disabled="!!errorMessage"></upload-button>
 
-						<b-button v-show="canClean" size="sm" variant="info" class="ml-2" @click="cleanUp">
-							<b-icon icon="filter-left"></b-icon> Clean Up
-						</b-button>
-					</div>
-				</template>
+                                <b-button v-show="canClean" size="sm" variant="info" class="ml-2" @click="cleanUp">
+                                    <b-icon icon="filter-left"></b-icon> Clean Up
+                                </b-button>
+                            </div>
+                        </template>
 
-				<b-alert :show="jobs.length === 0" variant="info" class="mb-0">
-					<b-icon icon="info-circle" class="mr-1"></b-icon> No Jobs available
-				</b-alert>
+                        <b-alert :show="jobs.length === 0" variant="info" class="mb-0">
+                            <b-icon icon="info-circle" class="mr-1"></b-icon> No Jobs available
+                        </b-alert>
 
-				<b-table v-show="jobs.length > 0" striped hover :fields="jobFields" :items="jobs" no-provider-paging :current-page="currentJobPage" :per-page="10" class="mb-0 job-table">
-					<template #cell(Filename)="{ item }">
-						<b-icon :icon="getJobIcon(item)" :icon-props="{ fontScale: 2 }"></b-icon>
-						{{ item.Filename }}
-					</template>
-					<template #cell(Hostname)="{ item }">
-						{{ getPrinterName(item.Hostname) }}
-					</template>
-					<template #cell(Progress)="{ item }">
-						<span v-if="!!item.ProgressText" v-text="item.ProgressText"></span>
-						<b-progress v-else-if="item.Progress !== null || item.TimeCompleted" :max="1" show-progress :animated="!item.TimeCompleted" :variant="getJobProgressVariant(item)">
-							<b-progress-bar :value="item.TimeCompleted ? 1 : item.Progress" :label="`${((item.TimeCompleted ? 1 : item.Progress) * 100).toFixed(1)} %`"></b-progress-bar>
-						</b-progress>
-					</template>
-					<template #cell(Time)="{ item }">
-						{{ formatTime(item) }}
-					</template>
-					<template #cell(ResumeRepeat)="{ item, index }">
-						<b-button v-if="item.Paused" size="sm" variant="success" @click="resumeFile(index)">
-							<b-icon icon="play-fill"></b-icon>
-						</b-button>
-						<b-button v-else-if="!!item.TimeCompleted" size="sm" variant="primary" @click="repeatFile(index)">
-							<b-icon icon="arrow-repeat"></b-icon>
-						</b-button>
-					</template>
-					<template #cell(PauseCancelDelete)="{ item, index }">
-						<b-button v-if="item.Paused" size="sm" variant="danger" @click="cancelFile(index)">
-							<b-icon icon="stop-fill"></b-icon>
-						</b-button>
-						<b-button v-else-if="!item.Hostname || !!item.TimeCompleted" size="sm" variant="danger" @click="deleteFile(index)">
-							<b-icon icon="trash"></b-icon>
-						</b-button>
-						<b-button v-else-if="item.Progress !== null" size="sm" variant="warning" :disabled="!!item.ProgressText" @click="pauseFile(index)">
-							<b-icon icon="pause"></b-icon>
-						</b-button>
-					</template>
-				</b-table>
+                        <b-table v-show="jobs.length > 0" striped hover :fields="jobFields" :items="jobs" no-provider-paging :current-page="currentJobPage" :per-page="10" class="mb-0 job-table">
+                            <template #cell(Filename)="{ item }">
+                                <b-icon :icon="getJobIcon(item)" :icon-props="{ fontScale: 2 }"></b-icon>
+                                {{ item.Filename }}
+                            </template>
+                            <template #cell(Hostname)="{ item }">
+                                {{ getPrinterName(item.Hostname) }}
+                            </template>
+                            <template #cell(Progress)="{ item }">
+                                <span v-if="!!item.ProgressText" v-text="item.ProgressText"></span>
+                                <b-progress v-else-if="item.Progress !== null || item.TimeCompleted" :max="1" show-progress :animated="!item.TimeCompleted" :variant="getJobProgressVariant(item)">
+                                    <b-progress-bar :value="item.TimeCompleted ? 1 : item.Progress" :label="`${((item.TimeCompleted ? 1 : item.Progress) * 100).toFixed(1)} %`"></b-progress-bar>
+                                </b-progress>
+                            </template>
+                            <template #cell(Time)="{ item }">
+                                {{ formatTime(item) }}
+                            </template>
+                            <template #cell(ResumeRepeat)="{ item, index }">
+                                <b-button v-if="item.Paused" size="sm" variant="success" @click="resumeFile(index)">
+                                    <b-icon icon="play-fill"></b-icon>
+                                </b-button>
+                                <b-button v-else-if="!!item.TimeCompleted" size="sm" variant="primary" @click="repeatFile(index)">
+                                    <b-icon icon="arrow-repeat"></b-icon>
+                                </b-button>
+                            </template>
+                            <template #cell(PauseCancelDelete)="{ item, index }">
+                                <b-button v-if="item.Paused" size="sm" variant="danger" @click="cancelFile(index)">
+                                    <b-icon icon="stop-fill"></b-icon>
+                                </b-button>
+                                <b-button v-else-if="!item.Hostname || !!item.TimeCompleted" size="sm" variant="danger" @click="deleteFile(index)">
+                                    <b-icon icon="trash"></b-icon>
+                                </b-button>
+                                <b-button v-else-if="item.Progress !== null" size="sm" variant="warning" :disabled="!!item.ProgressText" @click="pauseFile(index)">
+                                    <b-icon icon="pause"></b-icon>
+                                </b-button>
+                            </template>
+                        </b-table>
 
-				<b-pagination v-show="jobs.length > 10" v-model="currentJobPage" :total-rows="jobs.length" :per-page="10" align="fill" size="sm" class="my-0"></b-pagination>
-			</b-card>
+                        <b-pagination v-show="jobs.length > 10" v-model="currentJobPage" :total-rows="jobs.length" :per-page="10" align="fill" size="sm" class="my-0"></b-pagination>
+                    </b-card>
+                </b-col>
 
-			<b-card class="my-3" no-body>
-				<template #header>
-					<span>
-						<b-icon icon="printer"></b-icon> Printer Management
-					</span>
+                <b-col cols="4" class="pl-0">
+                    <b-card no-body>
+                        <template #header>
+                            <span>
+                                <b-icon icon="printer"></b-icon> Printer Management
+                            </span>
 
-					<add-printer-button :disabled="!!errorMessage"></add-printer-button>
-				</template>
+                            <add-printer-button :disabled="!!errorMessage"></add-printer-button>
+                        </template>
 
-				<b-alert :show="printers.length === 0" variant="warning" class="mb-0">
-					<b-icon icon="exclamation-triangle" class="mr-1"></b-icon> No Printers available
-				</b-alert>
+                        <b-alert :show="printers.length === 0" variant="warning" class="mb-0">
+                            <b-icon icon="exclamation-triangle" class="mr-1"></b-icon> No Printers available
+                        </b-alert>
 
-				<b-table v-show="printers.length > 0" striped hover :fields="printerFields" :items="printers" class="mb-0 printer-table">
-					<template #cell(Name)="{ item }">
-						<b-icon :icon="getPrinterIcon(item)"></b-icon>
-						{{ item.Name }}
-						<status-label :status="item.Status" class="ml-1"></status-label>
-					</template>
-					<template #cell(Online)="{ item }">
-						{{ `${item.Online ? 'Yes' : 'No'} ${item.Suspended ? ' (suspended)' : ''}` }}
-					</template>
-					<template #cell(SuspendResume)="{ item }">
-						<b-button v-if="item.Suspended" size="sm" variant="success" @click="resumePrinter(item.Hostname)">
-							<b-icon icon="play-fill"></b-icon>
-						</b-button>
-						<b-button v-else size="sm" variant="warning" @click="suspendPrinter(item.Hostname)">
-							<b-icon icon="pause"></b-icon>
-						</b-button>
-					</template>
-					<template #cell(Delete)="{ item }">
-						<b-button size="sm" variant="danger" @click="deletePrinter(item.Hostname)">
-							<b-icon icon="trash"></b-icon>
-						</b-button>
-					</template>
-				</b-table>
-			</b-card>
-		</b-container>
+                        <b-table v-show="printers.length > 0" striped hover :fields="printerFields" :items="printers" class="mb-0 printer-table">
+                            <template #cell(Name)="{ item }">
+                                <b-icon :icon="getPrinterIcon(item)"></b-icon>
+                                {{ item.Name }}
+                                <status-label :status="item.Status" class="ml-1"></status-label>
+                            </template>
+                            <template #cell(Online)="{ item }">
+                                {{ `${item.Online ? 'Yes' : 'No'} ${item.Suspended ? ' (suspended)' : ''}` }}
+                            </template>
+                            <template #cell(SuspendResume)="{ item }">
+                                <b-button v-if="item.Suspended" size="sm" variant="success" @click="resumePrinter(item.Hostname)">
+                                    <b-icon icon="play-fill"></b-icon>
+                                </b-button>
+                                <b-button v-else size="sm" variant="warning" @click="suspendPrinter(item.Hostname)">
+                                    <b-icon icon="pause"></b-icon>
+                                </b-button>
+                            </template>
+                            <template #cell(Delete)="{ item }">
+                                <b-button size="sm" variant="danger" @click="deletePrinter(item.Hostname)">
+                                    <b-icon icon="trash"></b-icon>
+                                </b-button>
+                            </template>
+                        </b-table>
+                    </b-card>
+                </b-col>
+            </b-row>
+        </b-container>
 	</div>
 </template>
 
@@ -185,8 +191,6 @@ export default {
 			printerFields: [
 				{ key: 'Name', sortable: true },
 				{ key: 'Hostname', sortable: true },
-				{ key: 'Online', sortable: true },
-				{ key: 'JobFile', formatter: (value) => value ? value : 'none', sortable: true },
 				{ key: 'SuspendResume', label: '', sortable: false },
 				{ key: 'Delete', label: '', sortable: false }
 			],
