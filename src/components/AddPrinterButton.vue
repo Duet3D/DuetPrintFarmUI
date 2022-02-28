07e9ecc608b1e35c19b8d2d2bed04af0ed46848b
Christian Hammacher <bmasterc@gmail.com>
2022-02-28 20:17:09 +0100

    Version 1.2.1
    Removed IE11 support
    Converted two remaining components to TS

--- src/components/AddPrinterButton.vue
+++ src/components/AddPrinterButton.vue
@@ -4,39 +4,31 @@
 			<b-icon icon="plus"></b-icon> Add Printer
 		</b-button>
 
-		<b-modal id="modal-add-printer" title="Add Printer" :ok-disabled="hostname == ''" @ok="ok">
+		<b-modal id="modal-add-printer" title="Add Printer" :ok-disabled="!hostname" @ok="ok">
 			<p>Please enter the hostname of the new printer:</p>
 			<b-form-input v-model="hostname" placeholder="Hostname or IP address" autofocus @keyup.enter="ok"></b-form-input>
 		</b-modal>
 	</div>
 </template>
 
-<script>
-'use strict'
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-import { addPrinter } from '../api/requests.ts'
+import { addPrinter } from '@/api/requests';
 
-export default {
-	props: {
-		disabled: {
-			default: false,
-			type: Boolean
-		}
-	},
-	data() {
-		return {
-			hostname: ''
-		}
-	},
-	methods: {
-		async ok() {
-			this.$bvModal.hide('modal-add-printer');
-			try {
-				await addPrinter(this.hostname);
-				this.hostname = '';
-			} catch (e) {
-				alert(`Failed to add printer:\n\n${e.message}`);
-			}
+@Component
+export default class AddPrinterButton extends Vue {
+	@Prop({ default: false }) readonly disabled!: boolean;
+
+	public hostname: string = "";
+
+	async ok() {
+		this.$bvModal.hide('modal-add-printer');
+		try {
+			await addPrinter(this.hostname);
+			this.hostname = '';
+		} catch (e) {
+			alert(`Failed to add printer:\n\n${e as string}`);
 		}
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import './components'
-import App from './App.vue'
+import './components';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
@@ -16,4 +16,3 @@ new Vue({
     el: '#app',
 	render: h => h(App)
 });
-
